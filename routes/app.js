0a851ba1f9796d7b50dd6524837379ec88c6f772
richyst <ricardo_1994@hotmail.com>
2018-02-23 16:32:24 -0600

    Progreso hasta chaptre 36

--- routes/app.js
+++ routes/app.js
@@ -1,25 +1,28 @@
 var express = require('express');
 var router = express.Router();
-var User = require('../models/user');
 
-router.get('/', function (req, res, next) {
-    User.findOne({}, function(err, doc) {
-        if(err) {
-            return res.send('Error!');
-        }
-        res.render('node', {email:doc.email});
-    });
-});
+// router.get('/', function (req, res, next) {
+//     User.findOne({}, function(err, doc) {
+//         if(err) {
+//             return res.send('Error!');
+//         }
+//         res.render('node', {email:doc.email});
+//     });
+// });
 
-router.post('/', function (req, res, next) {
-    var email = req.body.email;
-    var user = new User({
-        firstName: 'Ricardo',
-        lastName: 'Salcedo',
-        password: 'secret',
-        email: email
-    });
-    user.save();
-    res.redirect('/');
-});
+// router.post('/', function (req, res, next) {
+//     var email = req.body.email;
+//     var user = new User({
+//         firstName: 'Ricardo',
+//         lastName: 'Salcedo',
+//         password: 'secret',
+//         email: email
+//     });
+//     user.save();
+//     res.redirect('/');
+// });
+
+router.get('/', function(req, res, next) {
+    res.render('index');
+})
 module.exports = router;
